92078f49c08b0ad032c0fd47a740995505c8e2f3
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2024-06-01 19:32:34 +0530

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -1,7 +1,8 @@
 /*
-Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 Notice that the solution set must not contain duplicate triplets.
+
 Example 1:
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
@@ -42,8 +43,7 @@ class 3Sum {
                     else if(sum < 0)
                         j++;
                     else
-                        k--;
-                    
+                        k--; 
                 }
             }
         }
