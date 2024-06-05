adcb041cb3e1950b95fafd0f21aa03bb291482fd
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2024-06-05 19:11:04 +0530

    Update DuplicateZeros.java

--- DuplicateZeros.java
+++ DuplicateZeros.java
@@ -1,12 +1,11 @@
 /*
 Duplicate Zeros
+
 Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
 
 Note that elements beyond the length of the original array are not written.
 
 Do the above modifications to the input array in place, do not return anything from your function.
-
- 
 
 Example 1:
 
